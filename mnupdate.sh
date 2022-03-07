9fcc2333fff41cbf60f7476a2e1b638f61a171b1
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-03-07 03:17:55 -0500

    Update mnupdate.sh

--- mnupdate.sh
+++ mnupdate.sh
@@ -3,12 +3,12 @@ systemctl stop abet
 abet-cli stop
 cd /usr/local/bin
 rm -r {abet-cli,abetd,abet-tx,abet-qt}
-wget https://github.com/altbet/abet/releases/download/v3.4.1.0/abet-v3.4.1.0-linux.tar.gz
+wget https://github.com/MichaelHDesigns/abet/releases/download/v3.4.1.0/abet-v3.4.1.0-linux.tar.gz
 tar xvzf abet-v3.4.1.0-linux.tar.gz
 cd
 cd .abet/
 rm -r {budget.dat,fee_estimates.dat,peers.dat,backups,chainstate,sporks,banlist.dat,db.log,mncache.dat,blocks,debug.log,mnpayments.dat,zerocoin}
-wget https://github.com/altbet/abet/releases/download/v3.4.1.0/bootstrap.tar.gz
+wget https://github.com/MichaelHDesigns/abet/releases/download/v3.4.1.0/bootstrap.tar.gz
 tar xvzf bootstrap.tar.gz
 cd
 systemctl start abet
